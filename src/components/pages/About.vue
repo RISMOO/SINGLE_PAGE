693fe8e144be88c0abc22672f6e20edfe4837986
RISMOO <maurice.sider29@gmail.com>
2020-09-21 09:45:36 +0200

    second

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-<h1>A propos</h1>
+<h1 class="mt-5 text-muted"> A propos</h1>
+    <hr><br>
+
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic assumenda porro maiores nemo molestias minima inventore iusto temporibus quod, vero architecto, sed nobis adipisci explicabo animi laudantium, veniam quos esse.</p>
 <p>Voluptate voluptate culpa veniam aute.</p>
 <router-link to="/">
--- src/components/posts/Photo.vue
+++ src/components/posts/Photo.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-<h1>Photos</h1>
+<h1 class="mt-5 text-muted">Photo</h1>
+    <hr><br>
+
 <p>Et quis non veniam ea amet consectetur qui magna excepteur in qui culpa excepteur. Laboris laborum cillum nisi nostrud sint reprehenderit mollit culpa commodo magna. Quis minim exercitation cupidatat adipisicing cupidatat et adipisicing incididunt laborum sit labore quis aute. Enim laborum qui commodo sit incididunt ullamco sint nulla irure aliquip adipisicing.</p>
 <p>Duis reprehenderit mollit mollit non cupidatat. Consequat ex id labore voluptate commodo do incididunt voluptate fugiat dolore culpa cillum Lorem labore. Nisi ex et sit cillum eu fugiat aliquip sit proident ad ex ipsum.
 
